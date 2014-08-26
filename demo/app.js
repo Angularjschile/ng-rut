4ed7a92bd82c5185e7079aa97b080cd2d4935881
Administrador <gbelmm@gmail.com>
2014-08-26 13:07:11 -0400

    Initial commit

---
+++ demo/app.js
@@ -0,0 +1,6 @@
+var app;
+app=angular.module('demoapp',['ng-rut']);
+app.controller('demo',function($scope){
+
+$scope.rut='';
+});
---
+++ src/ngrut.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrador on 26/08/14.
+ */
+angular.module('ng-rut', [])
+    .directive('ngRut', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elem, attr, ctrl) {
+                var Fn={
+                    validaRut : function (rutCompleto) {
+                        if (!/^[0-9]+-[0-9kK]{1}$/.test( rutCompleto )) return false;
+                        var tmp = rutCompleto.split('-');
+                        if ( tmp[1] == 'K' ) tmp[1] = 'k';
+                        return (Fn.dv(tmp[0])) == tmp[1];
+                    },
+                    dv : function(T){
+                        var M=0,S=1;
+                        for(;T;T=Math.floor(T/10))
+                            S=(S+T%10*(9-M++%6))%11;
+                        return S?S-1:'k';
+                    }
+            };
+                scope.$watch(attr.ngModel, function(value) {
+                    if (value=='') return;
+
+                    if (!Fn.validaRut(value)) {
+                        ctrl.$rutValido=false;
+                        ctrl.$setValidity('rutValido',true);
+
+                    } else  ctrl.$setValidity('rutValido',false);
+                });
+
+
+            }
+        }
+    })
